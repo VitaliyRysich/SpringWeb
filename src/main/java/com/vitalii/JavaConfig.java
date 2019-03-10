f95e0c0d0e5980e12d17290c5e874f7344d121f9
vitaliyRysich <vitaliy.rysich@gmail.com>
2019-03-10 23:27:40 +0100

    Added xml and java configuration

--- src/main/java/com/vitalii/JavaConfig.java
+++ src/main/java/com/vitalii/JavaConfig.java
@@ -1,4 +1,18 @@
 package com.vitalii;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
 public class JavaConfig {
+
+    @Bean
+    public MessageProvider provider(){
+        return new SimpleMessageProvider();
+    }
+
+    @Bean
+    public MessageRenderer renderer(@Autowired MessageProvider provider){
+        return new SimpleMessageRenderer(provider);
+    }
+
 }
